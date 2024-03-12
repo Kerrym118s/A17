2f16d2f79fa5cdcb942877754509d626a192ea5a
Kerrym118s <163174221+Kerrym118s@users.noreply.github.com>
2024-03-13 00:15:39 +0300

    Update config.js

--- config.js
+++ config.js
@@ -24,7 +24,7 @@ global.prefa = ["."];                                    //Default prefix here. 
 global.Owner = ["94740158641"];         //If you want singal number so global.Owner = ['916297175943'] Change into your number.
 global.OwnerNumber = ["94740158641"];   //If you want singal number so global.Owner = ['916297175943'] Change into your number.
 global.ownertag = ["94740158641"];
-global.OwnerName = "Berrylove";
+global.OwnerName = "DarkRush";
 global.BotName = "A17";
 global.packname = "A17 Bot";                             //Do not change.
 global.author = "By: Kai";                               //Do not change.
