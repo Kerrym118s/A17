0edadea7775d5730f5d9113663fccb7ca5779795
Kerrym118s <163174221+Kerrym118s@users.noreply.github.com>
2024-03-13 11:58:40 +0300

    Update config.js

--- config.js
+++ config.js
@@ -25,9 +25,9 @@ global.Owner = ["94740158641"];         //If you want singal number so global.Ow
 global.OwnerNumber = ["94740158641"];   //If you want singal number so global.Owner = ['916297175943'] Change into your number.
 global.ownertag = ["94740158641"];
 global.OwnerName = "Berrylove";
-global.BotName = "A17";
+global.BotName = "Berry";
 global.packname = "A17 Bot";                             //Do not change.
-global.author = "By: Kai";                               //Do not change.
+global.author = "By: Berry";                               //Do not change.
 global.BotSourceCode = "https://github.com/Kai0071/A17"; //Do not change.
 global.SupportGroupLink = "https://chat.whatsapp.com/GhRGdwfaMVDCoeAdzILfl";
 
